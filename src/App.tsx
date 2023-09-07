32911d89b7ab808783d440beae3b330c21a5ed69
eLilyPad <elizabethbryce00@gmail.com>
2023-09-07 19:10:16 +0000

    7 Sept 2023, 20:10

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,43 @@
 import React, { useState, useEffect } from "react";
+// import Plotly from "plotly.js";
+// import createPlotlyComponent from "react-plotly.js/factory";
 import Plot from "react-plotly.js";
 import "./App.css";
+import { Data } from "plotly.js";
 
-function ListCountries() {
-  const [data, setData] = useState([{}]);
+// const Plot = createPlotlyComponent(Plotly);
 
-  useEffect(() => {
-    fetch("/countries")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
-  return (
-    <div>
-      {typeof data.countries === "undefined" ? (
-        <p>Loading...</p>
-      ) : (
-        data.countries.map((list, i) => <p key={i}>{list}</p>)
-      )}
-    </div>
-  );
-}
+// function ListCountries() {
+//   const [data, setData] = useState([{}]);
+
+//   useEffect(() => {
+//     fetch("/countries")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setData(data);
+//         console.log(data);
+//       });
+//   }, []);
+//   return (
+//     <div>
+//       {typeof data.countries === "undefined" ? (
+//         <p>Loading...</p>
+//       ) : (
+//         data.countries.map((list, i) => <p key={i}>{list}</p>)
+//       )}
+//     </div>
+//   );
+// }
 
 function ColumnPicker() {
-  const [selectedColumns, setSelectedColumns] = useState([]);
+  const [selectedColumns, setSelectedColumns] = useState<string[]>([]);
 
   const columns = [
     { key: "happiness_score", title: "Happiness Score" },
     { key: "freedom", title: "Freedom" },
     { key: "gdp_per_capita", title: "GDP per Capita" },
   ];
-  const handleSelectChange = (event) => {
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const options = [...event.target.selectedOptions];
     const values = options.map((o) => o.value);
 
@@ -65,29 +70,27 @@ function ColumnPicker() {
 }
 
 function TestTable() {
-  const [data, setData] = useState([]);
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("/table/Country-happiness_score/")
       .then((res) => res.json())
-      .then(setData);
+      .then(setTableData);
   }, []);
-
-  const getTable = () => {};
 
   const layout = {
     title: "Preview The Data",
   };
 
-  if (data === undefined) {
+  if (tableData === undefined) {
     console.log("API data is undefined, unable to build table");
     return "No table is available";
   }
 
-  const columns = Object.keys(data);
-  const tableRows = Object.values(data);
+  const columns = Object.keys(tableData);
+  const tableRows = Object.values(tableData);
 
-  const tableData = [
+  const data = [
     {
       type: "table",
       header: {
@@ -106,7 +109,8 @@ function TestTable() {
     },
   ];
 
-  return <Plot data={tableData} layout={layout} />;
+  // TODO: Plot table does not work with type script
+  // return <Plot data={data} layout={layout} />;
 }
 
 function App() {
