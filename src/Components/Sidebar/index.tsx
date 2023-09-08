413263b72fa2c70baf9325bc76f22ec85024e53f
eLilyPad <elizabethbryce00@gmail.com>
2023-09-08 21:02:28 +0100

    create file for Sidebar

---
+++ src/Components/Sidebar/index.tsx
@@ -0,0 +1,22 @@
+// import { Link, NavLink } from "react-router-dom";
+
+import React from "react";
+import "./index.scss";
+// import Logo from "../../assets/images/E_Logo.png";
+// import LogoSubtitle from "../../assets/images/LogoSubtitle.png";
+
+export default function Sidebar() {
+  return (
+    <div className="nav-bar">
+      {/* <Link className="logo" to="/">
+        <img src={Logo} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="elizabeth" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/"></NavLink>
+        <NavLink exact="true" activeclassname="active" to="/about"></NavLink>
+        <NavLink exact="true" activeclassname="active" to="/contact"></NavLink>
+      </nav> */}
+    </div>
+  );
+}
