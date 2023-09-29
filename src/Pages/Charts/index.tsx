40c1d6a18e3a7c166b3d7dd7427cbe3336bf2fda
eLilyPad <elizabethbryce00@gmail.com>
2023-09-29 17:32:15 +0000

    29 Sept 2023, 18:32

--- src/Pages/Charts/index.tsx
+++ src/Pages/Charts/index.tsx
@@ -1,5 +1,100 @@
 import React from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Pie,
+  PieChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+function testPieChart() {
+  const testData: any[] = [
+    {
+      name: "Group A",
+      value: 400,
+    },
+    {
+      name: "Group B",
+      value: 300,
+    },
+    {
+      name: "Group C",
+      value: 300,
+    },
+    {
+      name: "Group D",
+      value: 200,
+    },
+    {
+      name: "Group E",
+      value: 278,
+    },
+    {
+      name: "Group F",
+      value: 189,
+    },
+  ];
+  return (
+    <PieChart width={730} height={250}>
+      <Pie
+        data={testData}
+        dataKey="value"
+        nameKey="name"
+        cx="50%"
+        cy="50%"
+        outerRadius={50}
+        fill="#8884d8"
+      />
+    </PieChart>
+  );
+}
+
+function testLineChart() {
+  const testData: any[] = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+    },
+  ];
+  return (
+    <BarChart width={730} height={250} data={testData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="pv" fill="#8884d8" />
+      <Bar dataKey="uv" fill="#82ca9d" />
+    </BarChart>
+  );
+}
 
 export default function Charts() {
-  return <div></div>;
+  return (
+    <div>
+      {testPieChart()}
+      <br />
+      {testLineChart()}
+    </div>
+  );
 }
